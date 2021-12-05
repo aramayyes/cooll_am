88ef501091cd5845c7f1bf91434e804f6eb9f958
aramayyes <aramayis.amiraghyan@outlook.com>
2021-12-05 13:07:17 +0400

    Remove nested `README.md`.

--- Cooll/lib/utils/device_utils.dart
+++ Cooll/lib/utils/device_utils.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-/// Helper class for device related operations.
 class DeviceUtils {
   static void hideKeyboard() {
     FocusManager.instance.primaryFocus?.unfocus();
